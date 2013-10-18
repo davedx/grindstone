03a52e5d692a326681272d7b6f8f56076da2d419
Dave Clayton <daclayton@libertyglobal.com>
2013-10-18 15:48:53 +0200

    Wired up express to API. Now strangely not REST 2 but REST 3. Needs some verbs

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var api = require('./api.js'),
 	fs = require('fs'),
 	express = require('express');
 
-var app = express();
-
 var input = fs.readFileSync('api.json.txt');
 var json = JSON.parse(input);
 console.log("Generating API");
@@ -20,5 +18,29 @@ var user = {
 server.setUser(user);
 
 console.log(server);
+
 // add routes from API
-//for(var k in server)
+var routes = {};
+var app = express();
+
+var make_handler = function(fn) {
+	return function(req, res) {
+		//console.log("Invoking "+method+" on "+collection);
+		var response = fn(req);
+		res.setHeader('Content-Type: application/json');
+		res.send(response);
+	}
+};
+
+for(var collection in server.methods) {
+	for(var method in server.methods[collection]) {
+		var path = '/' + collection + '/' + method;
+		console.log("Adding route: " + path);
+
+		app.get(path, make_handler(server.methods[collection][method]));
+	}
+}
+
+var port = 8888;
+app.listen(port);
+console.log("Listening on "+port);
